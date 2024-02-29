9d4e8f3236e7e6a211ce264d67da6f55ff1d958e
Ayman Algamal <ayman01287985950@gmail.com>
2024-02-29 07:38:27 +0200

    Adding the simple check of coefficient signs at the beginning.

--- System_Stability_Check/Engine/engine.py
+++ System_Stability_Check/Engine/engine.py
@@ -1,3 +1,11 @@
-class Engine (object):
-    def __init__(self):
-        pass
+class RoutheEngine (object):
+    def __init__(self, coefficients_array):
+        self.coefficients_array = coefficients_array
+        self.degree = len(coefficients_array) - 1
+
+    def check_stability(self):
+        if(self.check_same_sign_coefficients(self.coefficients_array)):
+            return False
+        
+    def check_same_sign_coefficients(self, coefficients_array):
+        return all(x > 0 for x in coefficients_array) or all(x < 0 for x in coefficients_array)
--- System_Stability_Check/UnitTesting/engine_test.py
+++ System_Stability_Check/UnitTesting/engine_test.py
@@ -1,12 +1,36 @@
 
 import unittest
 import sys
+import os
+# Add the parent directory (project_root) to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from Engine.engine import RoutheEngine as Engine
 
 
 
 class TestEngine(unittest.TestCase):
-    pass
+    def test_constructor(self):
+        coefficients_array = [1, 2, 3, 4]
+        engine = Engine(coefficients_array)
+        self.assertEqual(engine.coefficients_array, coefficients_array)
+        self.assertEqual(engine.degree, 3)
+
+    def test_same_sign_coefficients_1(self):
+        coefficients_array = [1, 2, 3, 4]
+        engine = Engine(coefficients_array)
+        self.assertTrue(engine.check_same_sign_coefficients(coefficients_array))
+
+    def test_same_sign_coefficients_2(self):
+        coefficients_array = [-1, -2, -3, -4]
+        engine = Engine(coefficients_array)
+        self.assertTrue(engine.check_same_sign_coefficients(coefficients_array))
+
+    def test_same_sign_coefficients_3(self):
+        coefficients_array = [1, -2, 3, -4]
+        engine = Engine(coefficients_array)
+        self.assertFalse(engine.check_same_sign_coefficients(coefficients_array))
+        
     
 if __name__ == '__main__':
     unittest.main()
